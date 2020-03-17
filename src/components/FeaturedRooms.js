3225ee8a39b9097229e1fa76a99dff7e546bf858
jimhantrix <jimtekanyo@gmail.com>
2020-03-18 00:43:06 +0800

    Updated src file

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { Component} from "react"
 import { RoomContext } from "../context";
+import Loading from "./Loading";
+import Room from "./Room";
+import Title from "./Title";
 
 
 export default class FeaturedRooms extends Component{
 
-  static contextType = RoomContext
+    static contextType = RoomContext
 
-  render() {
-    
+    render() {
 
-    return <div> Feautred rooms</div>;
+      let { loading, feautureRooms: rooms } = this.context;
+
+      rooms = rooms.map(room => {
+        return<Room key ={rooms.id} room ={room}/>
+      })
+
+      return (
+        <section className ="featured-rooms">
+          <Title title ="featured rooms"/>
+            <div className ="featured-rooms-center">
+              { loading ? <Loading/> : rooms }
+            </div>
+
+        </section>
+
+        );
+    }
   }
-
-}
--- src/context.js
+++ src/context.js
@@ -1,29 +1,36 @@
 import React, { Component } from "react";
-import items from './data'
+import items from './data';
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
 
-state={
-rooms:[],
-sortedRooms:[],
-featuredRooms:[],
-loading:true
+state = {
+  rooms: [],
+  sortedRooms: [],
+  featuredRooms: [],
+  loading: true
 
 };
 //Data
 
 componentDidMount(){
-  let rooms = this.formatData(items)
+  let rooms = this.formatData(items);
+  let featuredRooms = rooms.filter(room => room.featured === true);
+  this.setState({
+    rooms,
+    featuredRooms,
+    sortedRooms: rooms,
+    loading: false
+  });
 }
 
 formatData(items){
   let tempItems =items.map(item =>{
-    let id = item.syst.id;
+    let id = item.sys.id
     let images =item.fields.images.map(image =>image.fields.file.url);
-    let room = { ...item.fields, images,id}
+    let room = {...item.fields,images,id}
     return room;
-  })
+  });
   return tempItems
 }
 
